b7ee4b3ae583bc6fc0e7aeb840c4b2778b23cff8
mosraful21 <mosraful21@gmail.com>
2022-11-08 22:02:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("service review is running");
+});
+
+app.listen(port, () => {
+    console.log(`service review server running on ${port}`);
+});
